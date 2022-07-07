59afd95a9c51ddde38f7bb1055faf7873823fdd3
JoeD_ <jd011904504@fer.hr>
2022-07-07 13:24:06 +0200

    completed app

--- server/database.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE Todo;
-
-CREATE TABLE todoTable(
-    todo_id SERIAL PRIMARY KEY,
-    description VARCHAR(255)
-);
--- server/index.js
+++ server/index.js
@@ -12,24 +12,64 @@ app.use(express.json());
 //create Todo
 app.post("/todos", async (req, res) => {
   try {
+    console.log(req.body);
     const { description } = req.body;
     const newTodo = await pool.query(
-      "INSERT INTO  todoTable (description) VALUES($1)",
+      "INSERT INTO  todotable (description) VALUES($1) RETURNING *",
       [description]
     );
-    res.json(newTodo);
+    res.json(newTodo.rows[0]);
   } catch (err) {
     console.error(err.message);
   }
 });
 
 //get all todos
+app.get("/todos", async(req, res)=>{
+  try{
+    const allTodos = await pool.query("SELECT * FROM todotable");
+    res.json(allTodos.rows);
+  }catch(err){
+    console.error(err.message);
+  }
+})
 
 //get 1 todo
 
+app.get("/todos/:id", async(req, res)=>{
+  try{
+    const {id} = req.params;
+    const todo = await pool.query("SELECT * FROM todotable WHERE todo_id = ($1)" , [id]);
+    res.json(todo.rows[0]);
+  }catch(err){
+    console.error(err.message);
+  }
+});
+
 //update 1 todo
+app.put("/todos/:id", async(req, res)=>{
+  try{
+    const {id} = req.params;
+    const {description} = req.body;
+    const updateTodo = await pool.query("UPDATE todotable SET description = $1 WHERE todo_id = $2", [description, id]);
+    res.json("Todo was updated");
+  }catch(err){
+    console.error(err.message);
+  }
+})
 
 //delete 1 todo
+
+app.delete("/todos/:id", async(req, res)=>{
+  try{
+    const {id} = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todotable WHERE todo_id = $1", [id]);
+    res.json("Todo was deleted");
+  }catch(err){
+    console.error(err.message);
+  }
+})
+
 
 app.listen(4200, () => {
   console.log("server started on port 4200");
