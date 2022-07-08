634744ba28aaf70f3d32aba797b71497030cc3a1
JoeD_ <jd011904504@fer.hr>
2022-07-08 11:55:42 +0200

    make it better

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
-
-const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
-
-const proConfig = "postgres://plpcqfkrtmrjlr:3ead3945576a2e1b0c1eb89d55990e926381e41a7f395dfa40a1e3974f9cb9c5@ec2-3-222-74-92.compute-1.amazonaws.com:5432/d9s7l4i954d422";
-console.log("db.js log: " + process.env.DATABASE_URL);
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString =
+  "postgres://plpcqfkrtmrjlr:3ead3945576a2e1b0c1eb89d55990e926381e41a7f395dfa40a1e3974f9cb9c5@ec2-3-222-74-92.compute-1.amazonaws.com:5432/d9s7l4i954d422";
 
 const pool = new Pool({
-  connectionString:
-    process.env.NODE_ENV === "production" ? proConfig : proConfig,
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
