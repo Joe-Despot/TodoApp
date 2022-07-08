90f81479c14dca4149a6a069702afa819b1709d4
JoeD_ <jd011904504@fer.hr>
2022-07-08 11:38:12 +0200

    .

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-const proConfig = "postgres://plpcqfkrtmrjlr:3ead3945576a2e1b0c1eb89d55990e926381e41a7f395dfa40a1e3974f9cb9c5@ec2-3-222-74-92.compute-1.amazonaws.com:5432/d9s7l4i954d422; //heroku addons";
+const proConfig = "postgres://plpcqfkrtmrjlr:3ead3945576a2e1b0c1eb89d55990e926381e41a7f395dfa40a1e3974f9cb9c5@ec2-3-222-74-92.compute-1.amazonaws.com:5432/d9s7l4i954d422";
 console.log("db.js log: " + process.env.DATABASE_URL);
 
 const pool = new Pool({
